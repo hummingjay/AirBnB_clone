9436366d36d8c322a1d9603b60cd38b60ec47d71
hummingjay <jays7mill@gmail.com>
2023-05-10 18:33:30 +0300

    Console

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ My AirBnB clone command interpreter """
+    prompt = "(hbnb) "
+
+    # THe commands
+    def do_EOF(self, line):
+        """Accepts interuption by user to exit program """
+        return True
+
+    def do_quit(self, line):
+        """Quit command to Exit the program """
+        return True
+
+if __name__ == '__main__':
+        HBNBCommand().cmdloop()
